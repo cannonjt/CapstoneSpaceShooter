a4c310f10896c30f91bdbac82be6ea0a1a1c6a11
Jake Cannon <cannonjt@plu.edu>
2015-01-28 13:29:35 -0800

    Basic Movement
    Added basic movement to the game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMax, xMin, zMax, zMin;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Boundary boundary;
+
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = speed * movement;
+
+		rigidbody.position = new Vector3
+			(
+				Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
+				0.0f,
+				Mathf.Clamp (rigidbody.position.y, boundary.zMin, boundary.zMax)
+			);
+
+	}
+}
