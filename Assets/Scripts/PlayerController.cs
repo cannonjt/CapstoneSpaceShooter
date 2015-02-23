7e41e14ef105b6678bdac7783275c108aced2f0d
Jake Cannon <cannonjt@plu.edu>
2015-02-23 13:19:42 -0800

    Start Ship rotation
    Beginning ship rotation scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,22 +11,35 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Boundary boundary;
-
+	public float tilt;
+	public float rotationSpeed;
+	public float moveHorizontal;
+	public float moveVertical;
 
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		//goes between -1 and 1 (-1 is left)
+		moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rigidbody.velocity = speed * movement;
+
+		//float rotationalAngle = Vector3.Angle(new Vector3(0.0f,
+
+		//goal: get rotation value between current vector and target vector
 
 		rigidbody.position = new Vector3
 			(
 				Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
 				0.0f,
-				Mathf.Clamp (rigidbody.position.y, boundary.zMin, boundary.zMax)
+				Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
 			);
+
+		//rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+
+
+		//rigidbody.rotation = Quaternion.Euler (0.0f, 
 
 	}
 }
