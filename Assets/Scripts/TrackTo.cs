d4bd3fe3edb99c19e6a999be4cde95c359b5e725
silvamj <madisonjsilva@gmail.com>
2015-03-02 13:09:52 -0800

    Added Enemy Rotation
    The enemy rotate toward the player if the player is with in 10 units of
    the enemy.

---
+++ Assets/Scripts/TrackTo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackTo : MonoBehaviour {
+
+	public float patrolSpeed = 1f;
+	public float chaseSpeed = 3f;
+	public float minDist = 10f;
+	public Transform target;
+	public float distance;
+
+
+	void Update(){
+		print ("Hello");
+		float potato = getDistance();
+
+		if(potato <= minDist) {
+
+			Vector3 track = target.position - transform.position;
+			print(track);
+
+			Quaternion wantDir = Quaternion.LookRotation( track, Vector3.up ); 
+			Quaternion newRotation = Quaternion.RotateTowards(rigidbody.rotation, wantDir, 60*Time.deltaTime);
+
+			rigidbody.MoveRotation (newRotation);
+		}
+	}
+
+	float getDistance(){
+
+		distance = Vector3.Distance (target.position, transform.position);
+
+		return distance;
+
+	}
+}
