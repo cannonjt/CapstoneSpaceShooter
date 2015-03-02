5ed9275c1ca699fad31664188a3ee51052ff6672
Jake Cannon <cannonjt@plu.edu>
2015-03-02 12:47:16 -0800

    Projectile Boundary
    The projectiles now delete themselves when the get too far from the
    ship.

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+	private GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 cameraPOS = camera.transform.transform.position;
+
+		transform.position = new Vector3(cameraPOS.x, 0, cameraPOS.z);
+	}
+}
---
+++ Assets/Scripts/PlayerProperties.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProperties : MonoBehaviour {
+
+	public int health;
+	public GameObject weapon;
+	public int shield;
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+		shield = 50;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
