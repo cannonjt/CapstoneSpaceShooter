2bd242a94d20bd6d9f1e0cf8aff1c952434e8a7b
cannonjt <cannonjt@plu.edu>
2015-03-02 16:54:21 -0800

    Follower Ship basics
    Made a follower ship that looks at closest enemy.

--- Assets/Scripts/Enemies/TrackTo.cs
+++ Assets/Scripts/Enemies/TrackTo.cs
@@ -6,9 +6,15 @@ public class TrackTo : MonoBehaviour {
 	public float patrolSpeed = 1f;
 	public float chaseSpeed = 3f;
 	public float minDist = 10f;
+	public float shootDist;
 	public Transform target;
 	public float distance;
 
+	void Start(){
+		if (target == null) {
+			target = FindClosestEnemy().transform;
+		}
+	}
 
 	void Update(){
 
@@ -17,7 +23,6 @@ public class TrackTo : MonoBehaviour {
 		if(howFar <= minDist) {
 
 			Vector3 track = target.position - transform.position;
-			print(track);
 
 			Quaternion wantDir = Quaternion.LookRotation( track, Vector3.up ); 
 			Quaternion newRotation = Quaternion.RotateTowards(rigidbody.rotation, wantDir, 60*Time.deltaTime);
@@ -32,5 +37,24 @@ public class TrackTo : MonoBehaviour {
 
 		return distance;
 
+	}
+
+	//Targets the closest enemy if no specified target
+	//from http://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html
+	GameObject FindClosestEnemy() {
+		GameObject[] gos;
+		gos = GameObject.FindGameObjectsWithTag("Enemy");
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = transform.position;
+		foreach (GameObject go in gos) {
+			Vector3 diff = go.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance) {
+				closest = go;
+				distance = curDistance;
+			}
+		}
+		return closest;
 	}
 }
