f364c1e698c88dbffd6667d7016ee8ae0bdbca3f
Neil Bateman <batemagn@plu.edu>
2015-04-08 05:05:31 -0700

    Simple Minimap
    Added a simple minimap using a second camera. Should look better with
    some additional UI elements around it.

---
+++ Assets/Resources/Scripts/Player/MinimapFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapFollow : MonoBehaviour {
+	
+	private GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 PlayerPOS = player.transform.transform.position;
+		GameObject.Find("MinimapCamera").transform.position = new Vector3(PlayerPOS.x, 10, PlayerPOS.z);
+		
+	}
+}
