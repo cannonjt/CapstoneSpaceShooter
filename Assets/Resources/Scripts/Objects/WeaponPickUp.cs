79c4413d4eccbcf83f06e120b2fa3f024e1e13b5
NikDee <devollnp@plu.edu>
2015-04-07 18:40:49 -0700

    See previous commit
    Git, I don't understand you sometimes. Are you telling me the previous
    commit went through WITHOUT THIS ONE CRUCIAL FILE SOMEHOW?!
    *grumble grumble*

--- Assets/Resources/Scripts/Objects/WeaponPickUp.cs
+++ Assets/Resources/Scripts/Objects/WeaponPickUp.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class WeaponPickUp : PickUp {
 
 	public Weapon containedWep;
+	public Sprite icon;
 
 	public override void doAction()
 	{
+		Image bulletIcon = GameObject.Find("BulletIcon").GetComponent<Image>();
+		bulletIcon.sprite = icon;
 		GameObject player = GameObject.FindGameObjectWithTag ("Player");
 		PlayerController playerController = (PlayerController)player.GetComponent (typeof(PlayerController));
 		playerController.changeWeapon (containedWep);
