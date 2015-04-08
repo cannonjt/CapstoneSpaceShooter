34263c8affbd80bca1bc2f9f3c8d56507ee2d784
Jake Cannon <cannonjt@plu.edu>
2015-04-07 20:31:09 -0700

    Boss Ship Shotgun
    Boss shoots out of the front of the ship, the shots should be slow
    enough to dodge if done correctly.

--- Assets/BossBehavior.cs
+++ Assets/BossBehavior.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class BossBehavior : MonoBehaviour {
 
+	public Weapon currentWep;
+	public float shootDist;
+
+	private Transform player;
+
 	// Use this for initialization
 	void Start () {
-	
+		currentWep = (Weapon)Instantiate (currentWep);
+		currentWep.GetComponent<Weapon> ().setUp (gameObject);
+		currentWep.transform.parent = transform;
+
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (getDistanceToPlayer () < shootDist) {
+				currentWep.shoot ();
+		}
+	}
+
+	//Calculates the distance between the target and its self
+	float getDistanceToPlayer(){
+		float distance;
+		distance = Vector3.Distance (player.position, transform.position);
+		return distance;
 	}
 }
