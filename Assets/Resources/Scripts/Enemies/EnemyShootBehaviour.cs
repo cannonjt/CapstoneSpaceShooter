10c525b81a1c984d351ec988da6e3e19fa163e85
silvamj <madisonjsilva@gmail.com>
2015-04-10 16:22:47 -0700

    Enemy Shooting
    Added left and right weapon to enemy
    Added enemy shooting (needs work)
    Updated the Enemy Prefab

--- Assets/Resources/Scripts/Enemies/EnemyShootBehaviour.cs
+++ Assets/Resources/Scripts/Enemies/EnemyShootBehaviour.cs
@@ -3,18 +3,26 @@ using System.Collections;
 
 public class EnemyShootBehaviour : MonoBehaviour {
 
-	public Weapon currentWep;
+	public Weapon currentWepLeft;
+	public Weapon currentWepRight;
 	public float shootDist;
 
 	// Use this for initialization
 	void Start () {
-		currentWep = (Weapon)Instantiate (currentWep);
-		currentWep.GetComponent<Weapon> ().setUp (gameObject);
-		currentWep.transform.parent = transform;
+		currentWepLeft = (Weapon)Instantiate (currentWepLeft);
+		currentWepLeft.GetComponent<Weapon> ().setUp (gameObject);
+		currentWepLeft.transform.parent = transform;
+
+		currentWepRight = (Weapon)Instantiate (currentWepRight);
+		currentWepRight.GetComponent<Weapon> ().setUp (gameObject);
+		Transform rightSpawn = transform.GetChild (1);
+		currentWepRight.GetComponent<Weapon> ().setSpawnLocation (rightSpawn);
+		currentWepRight.transform.parent = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		currentWepLeft.GetComponent<Weapon> ().shoot();
+		currentWepRight.GetComponent<Weapon> ().shoot();
 	}
 }
