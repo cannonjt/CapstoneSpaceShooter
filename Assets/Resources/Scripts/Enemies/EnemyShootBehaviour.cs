5a103254d4f9bb1b579e67b436a957502a3038e6
silvamj <madisonjsilva@gmail.com>
2015-04-11 15:54:51 -0700

    Asteroid Health
    The asteroid has health and will be destroyed if shot at enough.
    
    "finished" Enemy Shoot Behavior

--- Assets/Resources/Scripts/Enemies/EnemyShootBehaviour.cs
+++ Assets/Resources/Scripts/Enemies/EnemyShootBehaviour.cs
@@ -11,7 +11,7 @@ public class EnemyShootBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		nextCheck = 0;
+		nextCheck = Time.time + Random.Range (0.0f,1.0f);
 		currentWepLeft = (Weapon)Instantiate (currentWepLeft);
 		currentWepLeft.GetComponent<Weapon> ().setUp (gameObject);
 		currentWepLeft.transform.parent = transform;
@@ -27,14 +27,21 @@ public class EnemyShootBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > nextCheck) {
-			int shoot = Random.Range (0,10);
+		Transform player = getTarget ();
+		if (player.gameObject.activeInHierarchy) {
+			if (Time.time > nextCheck) {
+				int shoot = Random.Range (0, 10);
 
-			if (shoot > 4) {
-				currentWepLeft.GetComponent<Weapon> ().shoot ();
-				currentWepRight.GetComponent<Weapon> ().shoot ();
+				if (shoot > 4) {
+					currentWepLeft.GetComponent<Weapon> ().shoot ();
+					currentWepRight.GetComponent<Weapon> ().shoot ();
+				}
+				nextCheck += fireRate;
 			}
-			nextCheck += fireRate;
 		}
 	}
+
+	Transform getTarget(){
+			return gameObject.GetComponent<TrackTo>().target;
+		}
 }
---
+++ Assets/Resources/Scripts/Objects/AsteroidHealth.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidHealth : MonoBehaviour {
+
+	public float health;
+	public float maxHealth;
+	public GameObject explosion;
+	
+	[HideInInspector]
+	public bool invToPirece;
+	[HideInInspector]
+	public float invLength;
+	
+	// Use this for initialization
+	void Start () {
+		health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Projectile") 
+		{
+			//Projectiles have damagers. I know, workaroundy, but best I could do
+			GameObject bullet = other.gameObject;
+			Damager theThing = bullet.GetComponent<Damager>();
+			if (!theThing.getDoH ())
+			{
+				//check if we're inv
+				if (!invToPirece)
+				{
+					takeDamage(theThing.getDamage());
+					invToPirece = true;
+					invLength = Time.time + 1;
+				}
+			}
+			else
+			{
+				takeDamage (theThing.getDamage());
+				Destroy (other.gameObject);
+			}
+		}
+	}
+	
+	public void takeDamage(float damage)
+	{
+		health -= damage;
+		if (health <= 0) {
+			Destroy (gameObject);
+			if(explosion != null)
+				Instantiate (explosion, transform.position, transform.rotation);
+		} 
+	}
+
+}
