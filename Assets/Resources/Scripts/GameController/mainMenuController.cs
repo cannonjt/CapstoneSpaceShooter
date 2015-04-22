1e3b0f051c4b46bc058b09feb4827b8b469b4537
Neil Bateman <batemagn@plu.edu>
2015-04-21 18:15:00 -0700

    Basic Main Menu Added
    A basic main menu with the ability to move to the play scene and to quit
    the application was added.

---
+++ Assets/Resources/Scripts/GameController/mainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenuController : MonoBehaviour {
+	public GameObject playButton;
+	public GameObject quitButton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void playButtonClicked(){
+		Application.LoadLevel("Level1");
+	}
+	public void quitButtonClicked(){
+		Application.Quit ();
+	}
+}
