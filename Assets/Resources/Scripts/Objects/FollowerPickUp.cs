8ffcc87119bfe666c0d48a051c2ff984ed810ce1
silvamj <madisonjsilva@gmail.com>
2015-04-25 13:52:04 -0700

    Finished Follower Pick up
    Finished the Script
    Finished the Model
    Created a Prefab

---
+++ Assets/Resources/Scripts/Objects/FollowerPickUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowerPickUp : PickUp {
+
+	
+	public override void doAction()
+	{
+		//Finds the player that is being followed
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+
+		//Creates the Follower, position of follower
+		GameObject spawnFollower = (GameObject)Resources.Load ("Prefabs/Player/Follower");
+		Vector3 spawnPos = player.transform.position;
+		GameObject newFollower = (GameObject)Instantiate (spawnFollower, spawnPos, Quaternion.identity);
+
+		//set the leader of follower
+		FollowerBehavior fBeh = newFollower.GetComponent<FollowerBehavior> ();
+		fBeh.leader = player.transform;
+	}
+}
